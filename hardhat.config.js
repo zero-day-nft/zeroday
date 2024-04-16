247fa7560700343820b938b00fd3b763c52e82a0
soheil-vafaei <soheil.vafaei.official@gmail.com>
2024-04-16 17:17:47 +0330

    fix hardhat config for tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: ["f389c0a3163c73e4a0476a96d292c06cc9b1ff8be989b7273bb72c577dfed494"]
+    }
+  }
 };
