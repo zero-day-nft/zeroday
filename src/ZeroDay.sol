b4f7415250a7818984e3ab5e87ba0c1881574109
ParsaAminpour <parsa.aminpour@gmail.com>
2024-05-21 16:18:50 +0330

    commit for adding external libraries

---
+++ src/ZeroDay.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+// import { Math } from "@openzeppelin/contracts/"
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract ZeroDay {
+
+}
