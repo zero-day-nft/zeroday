b7dbc4ca3a24c2f5e59a29984990c138ed83a8f0
ParsaAminpour <parsa.aminpour@gmail.com>
2024-06-04 16:48:03 +0330

    removing last version of ZeroDay NFT collection

--- last-version/ignition/modules/Lock.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("Apollo", (m) => {
-  const apollo = m.contract("Rocket");
-
-  m.call(apollo, "launch");
-
-  return { apollo };
-});
